5feb87a8590dc7d5eac330ecbe74e19131bb6d80
Mr. Holmes <81588567+Gamerdudee@users.noreply.github.com>
2025-07-05 23:27:32 -0600

    Update spdx-patcher.cjs

--- spdx-patcher.cjs
+++ spdx-patcher.cjs
@@ -10,7 +10,7 @@ const path = require('path');
 const crypto = require('crypto');
 
 const skipDirs = ['node_modules', 'mnt/data', '.git', '.github'];
-const HASH_LABEL = 'Hash Reference (Integrity Binding):';
+const HASH_LABEL = 'Hash (Integrity Binding):';
 
 const trackedExtensions = {
   '.js': '// SPDX-License-Identifier: Declaratory-Royalty',
